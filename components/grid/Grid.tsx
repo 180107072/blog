60bde0a6be0787236f5fe60ab49678bb2bed1d9a
180107072 <iokyufokyu@gmail.com>
2023-10-22 05:19:46 +0600

    fix: grid

--- components/grid/Grid.tsx
+++ components/grid/Grid.tsx
@@ -1,60 +1,39 @@
-import {
-  FC,
-  PropsWithChildren,
-  startTransition,
-  useEffect,
-  useId,
-  useRef,
-  useState,
-} from "react";
-import { GridTile } from "@/components/grid/Tile";
-import { useGridScale } from "@/hooks/use-grid-scale";
+import { GRID_SIZE, GRID_TILES, NUM_OF_TILES } from '@/config/grid';
+import { useGridScale } from '@/hooks/use-grid-scale';
+import React, { FC, PropsWithChildren, useRef, useState, useEffect, startTransition } from 'react';
+import { GridTile } from './Tile';
+import { getGridTranslation } from '@/utils/get-grid-translation';
 
-import { GRID_SIZE, GRID_TILES, NUM_OF_TILES } from "@/config/grid";
+import css from './Grid.module.scss'
 
-import css from "./Grid.module.scss";
-import { getGridTranslation } from "@/utils/get-grid-translation";
-
-const limit = 10;
-
-const Renderer: FC<{
-  chunks: JSX.Element[];
-  onRendered: (id: string) => void;
-}> = ({ chunks, onRendered }) => {
-  const scope = useId();
-  useEffect(() => onRendered(scope), [onRendered, scope]);
-  return <>{chunks}</>;
-};
+const limit = 15;
 
 export const Grid: FC<PropsWithChildren> = ({ children }) => {
   const chunk = useRef(1);
+  const [tiles, setTiles] = useState<JSX.Element[]>([]);
   const grid = useRef([...GRID_TILES]);
 
   const scale = useGridScale(GRID_SIZE);
+  const to = chunk.current * limit;
 
-  const [tiles, setTiles] = useState<JSX.Element[]>([]);
+  const load = () => {
+    if (tiles.length >= NUM_OF_TILES || grid.current.length === 0) return;
 
-  const load = (id: string) => {
-    const to = chunk.current * limit;
+    const slicedTiles = grid.current.splice(0, to);
+    const mappedTiles = slicedTiles.map((tile) => <GridTile key={tile} />);
 
-    if (tiles.length >= NUM_OF_TILES) return;
-
-    const mapped = grid.current
-      .splice(0, to)
-      .map((tile) => <GridTile key={`${id}-${tile}-${to}`} />);
-
-    setTiles((prev) => [...prev, ...mapped]);
+    setTiles((prev) => prev.concat(mappedTiles));
     chunk.current += 1;
   };
 
+  useEffect(() => {
+    startTransition(load);
+  }, [tiles]);
+
   return (
     <div className={css.gridLayout}>
-      <div
-        className={css.gridContainer}
-        style={{ transform: getGridTranslation(scale) }}
-      >
+      <div className={css.gridContainer} style={{ transform: getGridTranslation(scale) }}>
         {children}
-        <Renderer onRendered={load} chunks={tiles} />
         {tiles}
       </div>
     </div>
--- config/grid.ts
+++ config/grid.ts
@@ -1,4 +1,4 @@
 export const GRID_SIZE = { width: 2500, height: 4500 };
-export const NUM_OF_TILES = 400;
+export const NUM_OF_TILES = 800;
 export const GRID_TILES = [...Array(NUM_OF_TILES).keys()];
 export const GRID_TILE_COUNT = [1, 2, 3, 4];
